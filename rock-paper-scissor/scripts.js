57414099c2a93f43ff9a81bd83a20d4909888635
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-04-29 18:09:55 -0400

    Validation of input and input popup window

--- rock-paper-scissor/scripts.js
+++ rock-paper-scissor/scripts.js
@@ -11,104 +11,110 @@ var lossarray = [0];
 
 function playGame() {
 
-function userSelection() {
-    var userChoice = prompt("Enter R for Rock, S for Scissor, or P for Paper");
-    return userChoice;
-}
+    function userSelection() {
+        var userChoice = prompt("Please enter one letter: R, S or P \n R for Rock \n S for Scissor \n P for Paper");
+         if (userChoice !== "R" && userChoice !== "S" && userChoice !== "P") {
+            return userSelection();
+        } else {
+            return userChoice;
+        }
+    }   
 
-function computerSelection() {
-    var index = (Math.floor(Math.random() * 3));
-    var options = ["R", "S", "P"];
-    alert("The computer chose: " + options[index]);
-    return options[index];
-}
-
-function declareWinner() {
-    if (userChoice === computerChoice) {
-        return "It's a tie!";
-    };
-    if (userChoice === "R" && computerChoice === "S") {
-        return "😁 You are a winner! ";
-    };
-    if (userChoice === "R" && computerChoice === "P") {
-        return "☹️ You lose!";
-    };
-    if (userChoice === "P" && computerChoice === "S") {
-        return "☹️ You lose!";
-    };
-    if (userChoice === "P" && computerChoice === "R") {
-        return "😁 You are a winner!";
-    };
-    if (userChoice === "S" && computerChoice === "R") {
-        return "☹️ You lose!";
-    };
-    if (userChoice === "S" && computerChoice === "P") {
-        return "😁 You are a winner!";
+    function computerSelection() {
+        var index = (Math.floor(Math.random() * 3));
+        var options = ["R", "S", "P"];
+        alert("The computer chose: " + options[index]);
+        return options[index];
     }
-}
 
-function countwins() {
-    if (winner.includes(winnertext)) {
-      return winsarray.push(1);   
-    } else {
-        return winsarray.push(0);
+    function declareWinner() {
+        if (userChoice === computerChoice) {
+            return "It's a tie!";
+        };
+        if (userChoice === "R" && computerChoice === "S") {
+            return "😁 You are a winner! ";
+        };
+        if (userChoice === "R" && computerChoice === "P") {
+            return "☹️ You lose!";
+        };
+        if (userChoice === "P" && computerChoice === "S") {
+            return "☹️ You lose!";
+        };
+        if (userChoice === "P" && computerChoice === "R") {
+            return "😁 You are a winner!";
+        };
+        if (userChoice === "S" && computerChoice === "R") {
+            return "☹️ You lose!";
+        };
+        if (userChoice === "S" && computerChoice === "P") {
+            return "😁 You are a winner!";
+        }
     }
-}
 
-function countloss() {
-    if (winner.includes(losetext)) {
-       return lossarray.push(1);
-    }  else {
-        return lossarray.push(0);
-    }  
-}
-
-function wintotal() {
-    let sum = 0;
-    for (var i = 0; i < winsarray.length; i++) {
-     sum += winsarray[i];
+    function countwins() {
+        if (winner.includes(winnertext)) {
+            return winsarray.push(1);
+        } else {
+            return winsarray.push(0);
+        }
     }
-    return sum;
-}
 
-function losstotal() {
-    let sum = 0;
-    for (var i = 0; i < lossarray.length; i++) {
-     sum += lossarray[i];
-    } 
-     return sum;
-}
-
-function scores() {
-    alert("Wins: " + totalwins +"\nLosses: " + totallosses );
-    if (confirm("Do you want to play again?")) {
-       playGame();
-    } else {
-        document.getElementById("thankyou").innerHTML = "Thanks for playing!  <br><br>If you'd like to play again, reload the page."
+    function countloss() {
+        if (winner.includes(losetext)) {
+            return lossarray.push(1);
+        } else {
+            return lossarray.push(0);
+        }
     }
+
+    function wintotal() {
+        let sum = 0;
+        for (var i = 0; i < winsarray.length; i++) {
+            sum += winsarray[i];
+        }
+        return sum;
+    }
+
+    function losstotal() {
+        let sum = 0;
+        for (var i = 0; i < lossarray.length; i++) {
+            sum += lossarray[i];
+        }
+        return sum;
+    }
+
+    function scores() {
+        alert("Wins: " + totalwins + "\nLosses: " + totallosses);
+        if (confirm("Do you want to play again?")) {
+            playGame();
+        } else {
+            document.getElementById("thankyou").innerHTML = "Thanks for playing!  <br><br>If you'd like to play again, reload the page."
+        }
+    }
+
+    var userChoice = userSelection();
+    var computerChoice = computerSelection();
+    var winner = declareWinner();
+
+    declareWinner(userChoice, computerChoice);
+    alert("This round: " + winner);
+
+    var winnertext = "winner";
+    var losetext = "lose";
+    var wins = countwins();
+    var loss = countloss();
+    var totalwins = wintotal();
+    var totallosses = losstotal();
+    var totalscores = scores();
+
+    console.log(winsarray);
+    console.log(lossarray);
+    console.log(totalwins);
+    console.log(totallosses);
+    console.log(wins);
+    console.log(loss);
 }
 
-var userChoice = userSelection().toUpperCase;
-var computerChoice = computerSelection();
-var winner = declareWinner();
 
-declareWinner(userChoice, computerChoice);
-alert("This round: " + winner);
-
-var winnertext = "winner";
-var losetext = "lose";
-var wins = countwins();
-var loss = countloss();
-var totalwins = wintotal();
-var totallosses = losstotal();
-var totalscores = scores();
-     
-console.log(winsarray);
-console.log(lossarray);
-console.log(totalwins);
-console.log(totallosses);
-console.log(wins);
-console.log(loss);
-}
-
+    
 playGame();
