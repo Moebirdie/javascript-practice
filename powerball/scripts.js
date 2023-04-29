7430afb1f92a9844afd729f83adcb016dcd61b8e
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-04-29 18:54:57 -0400

    Powerball framework

---
+++ powerball/scripts.js
@@ -0,0 +1,57 @@
+//players choose powerball numbers - five numbers from 1 to 69  and a powerball number cost:$2 
+//players choose a powerball number - one from 1 to 26 included above for a total of 6 numbers to match
+//Power play multiplyer - multiplies winnings by 2, 3,4,5 or 10
+    //jackpot not affected by multiplier
+    //never higher than 2 for 2nd prize
+    //picked out of hat before the main drawing of the 6 numbers
+//prizes
+    //jackpot - match all 6 numbers - prize: full amount
+    //match 5 numbers - prize: 1 million
+    //match 4 numbers + powerball - 50,000
+    //match four numbers - 100
+    //match three numbers = powerball - 100
+    //match three numbers - 7
+    //match two numbers + powerball - 7
+    //match one number + powerball - 4
+    //match powerball - 4
+
+
+    function playerNumbers() {
+
+    }
+    
+    function playerPowerball() {
+
+    } 
+
+    function pbMultiplier() {
+
+    }
+
+    function pbNumbers() {
+
+    }
+    function pbPowerball() {
+
+    }
+
+    playerNumArray = [];
+    pbNumArray = []; 
+    
+    var playerNums = playerNumbers();
+    var playerPbNum = playerPowerball();
+    var Multiplier = pbMultiplier();
+    var pbNums = pbNumbers();
+    var pbPowerBall = pbPowerball(); 
+
+    
+    var jackpot = "51 million dollars";
+    var fiveNumMatch = "1 million dollars";
+    var fourNumPlusMatch = "50,000 dollars";
+    var fourNumMatch = "100 dollars";
+    var threeNumPlusMatch = "100 dollars";
+    var threeNumMatch = "7 dollars";
+    var twoNumPlusMatch = "7 dollars";
+    var oneNumPlusMatch = "4 dollars";
+    var powerballOnly = "4 dollars";
+
